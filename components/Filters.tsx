a162ac8a22880a05cd657dc920f95ce0e4ff0b6c
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-12-24 18:09:52 +0500

    Complete

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,18 +1,25 @@
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
 import { categories } from '@/constants/data';
 
 const Filters = () => {
   const params = useLocalSearchParams<{filter?:string}>();
   const [selectedCategory, setSelectedCategory] = useState(params.filter || 'All'); 
-  const handleCategoryPress = (category:string)=>{
+  const handleCategoryPress = (category: string) => {
+    if (selectedCategory === category) {
+      setSelectedCategory("All");
+      router.setParams({ filter: "All" });
+      return;
+    }
+
     setSelectedCategory(category);
-  }
+    router.setParams({ filter: category });
+  };
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false} className='mt-3 mb-2'>
       {categories.map((category, index) =>(
-        <TouchableOpacity onPress={()=>handleCategoryPress(category.category)} className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCategory === category.category? 'bg-primary-300' : 'bg-primary-100 border border-primary-200'}`}>
+        <TouchableOpacity key={index  } onPress={()=>handleCategoryPress(category.category)} className={`flex flex-col items-start mr-4 px-4 py-2 rounded-full ${selectedCategory === category.category? 'bg-primary-300' : 'bg-primary-100 border border-primary-200'}`}>
           <Text className={`text-sm ${selectedCategory === category.category ? "text-white font-rubik-bold mt-0.5": "text-black-300 font-rubik"}`}>{category.title}</Text>
         </TouchableOpacity>
       ))}
